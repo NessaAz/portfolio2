e5a79ee137aa4fdefede90bb2633d47c85bff20d
NessaAz <vanessa.azenwa@gmail.com>
2022-10-03 19:05:53 +0300

    add skills and(qualifications)

--- script.js
+++ script.js
@@ -19,6 +19,8 @@ if(navClose){
     })
 }
 
+
+
 //REMOVE MENU ON MOBILE
 const navLink = document.querySelectorAll('.nav-link')
 
@@ -30,10 +32,54 @@ function linkAction() {
 navLink.forEach(n=> n.addEventListener('click', linkAction))
 
 
+
 //ACCORDION SKILLS
+const skillsContent = document.getElementsByClassName('skills-content'),
+      skillsHeader = document.querySelectorAll('.skills-header')
+
+function toggleSkills() {
+    let itemClass = this.parentNode.className
+
+    for ( i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = 'skills-content skills-close'
+    }
+    if(itemClass === 'skills-content skills-close'){
+        this.parentNode.className = 'skills-content skills-open'
+    }
+}      
+
+skillsHeader.forEach((el) =>{
+    el.addEventListener('click', toggleSkills)
+})
+
 
 //QUALIFICATION TABS
+// const tabs = document.querySelectorAll('[data-target]'),
+//       tabContents = document.querySelectorAll('[data-content]')
+
+// tabs.forEach(tab =>{
+//     tab.addEventListener('click', () =>{
+//         const target = document.querySelector(tab.dataset.target)
+
+//         tabContents.forEach(tabContent =>{
+//             tabContent.classList.remove('qualification-active')
+//         })
+
+//         target.classList.add('qualification-active')
+
+//         tabs.forEach(tab =>{
+//             tab.classList.remove('qualification-active')
+//         })
+//         tab.classList.add('qualification-active')
+//     })
+// })      
 
 //SERVICES MODAL
 
-//
+//PORTFOLIO SWIPER
+
+//TESTIMONIAL 
+
+//SCROLL SECTIONS ACTIVE LINK
+
+//BACKGROUND HEADER CHANGE
