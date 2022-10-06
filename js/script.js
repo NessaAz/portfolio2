57025f0748ce81bf073dee4b82f854fe6621e937
NessaAz <vanessa.azenwa@gmail.com>
2022-10-06 14:33:48 +0300

    add form action: formspree, fix horizontal scroll

--- js/script.js
+++ js/script.js
@@ -174,3 +174,68 @@ themeButton.addEventListener('click', () =>{
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+
+
+//contact form
+const thename = document.getElementById('name');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+const contactForm = document.getElementById('contact-form');
+const errorElement = document.getElementById('error');
+const successMsg = document.getElementById('success-msg');
+const submitBtn = document.getElementById('submit');
+  
+const validate = (e) => {
+  e.preventDefault();
+ 
+  if (thename.value.length < 3) {
+    errorElement.innerHTML = 'Your name should be at least 3 characters long.';
+    return false;
+  } 
+  
+  if (!(email.value.includes('.') && (email.value.includes('@')))) {
+    errorElement.innerHTML = 'Please enter a valid email address.';
+    return false;
+  } 
+
+  if (!emailIsValid(email.value)) {
+    errorElement.innerHTML = 'Please enter a valid email address.';
+    return false;
+  }
+
+  if (message.value.length < 15) {
+    errorElement.innerHTML = 'Please write a longer message.';
+    return false;
+  }
+
+  errorElement.innerHTML = '';
+  successMsg.innerHTML = 'Thank you! I will get back to you as soon as possible.'; 
+
+  e.preventDefault();
+  setTimeout(function () {
+    successMsg.innerHTML = '';
+    document.getElementById('contact-form').reset();
+  }, 6000);
+
+  return true;
+
+}
+
+const emailIsValid = email => {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+submitBtn.addEventListener('click', validate);
+
+
+/*Pop up alert*/
+$('button#submit').click(function(){
+    let name=$('input#name').val()
+    if(name==""){
+        alert("Please fill the form")
+    }
+    else {
+        alert(`${name}, I have received your message.Thank you for reaching out.`)
+    }
+})
